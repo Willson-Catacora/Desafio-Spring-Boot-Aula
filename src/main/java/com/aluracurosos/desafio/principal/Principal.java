a0bc9e4d505ba7ddf7b7bddcc2e4b67ee7f67401
willson <willsoncatacora1@gmail.com>
2024-10-21 20:58:15 -0300

    title search.

--- src/main/java/com/aluracurosos/desafio/principal/Principal.java
+++ src/main/java/com/aluracurosos/desafio/principal/Principal.java
@@ -1,12 +1,13 @@
 package com.aluracurosos.desafio.principal;
 
 import com.aluracurosos.desafio.model.Datos;
+import com.aluracurosos.desafio.model.DatosAutor;
 import com.aluracurosos.desafio.model.DatosLibro;
 import com.aluracurosos.desafio.service.ConsumirAPI;
 import com.aluracurosos.desafio.service.ConvierteDatos;
 
-import java.util.Comparator;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Principal {
     private Scanner sc = new Scanner(System.in);
@@ -16,6 +17,7 @@ public class Principal {
     private ConvierteDatos convierteDatos = new ConvierteDatos();
     private boolean sw = true;
     private Integer opcion;
+    private String texto;
 
     public void mostrarMenu() {
 //        System.out.println("hola mundo desde sprin boot");
@@ -29,11 +31,15 @@ public class Principal {
             System.out.println("4) Salir");
             System.out.println("================================================");
             opcion = sc.nextInt();
+            sc.nextLine();
             switch (opcion) {
                 case 1:
                     top10();
                     break;
                 case 2:
+                    System.out.println("\nIngrese el titulo del libro que quiera buscar");
+                    texto = sc.nextLine();
+                    busquedaTitulo(texto);
                     break;
                 case 3:
                     break;
@@ -58,5 +64,31 @@ public class Principal {
                 .forEach(e -> {
                     System.out.println("\t" + e.tiutlo() + ",\t Descargados: " + e.descargas());
                 });
+    }
+
+    public void busquedaTitulo(String search) {
+//        System.out.println(search);
+        var json = consumoAPI.obtenerDatos(URL_BASE + "?search=" + search.toLowerCase().replace(" ", "%20"));
+        var datos = convierteDatos.obtenerDatos(json, Datos.class);
+
+        Optional<DatosLibro> libro = datos.libros().stream()
+                .sorted(Comparator.comparing(DatosLibro::descargas).reversed())
+                .findFirst();
+        if (libro.isPresent()) {
+            System.out.println("Libro Encontrado Exitosamente....");
+            System.out.println(" Titulo : " + libro.get().tiutlo() + "\n Autores :");
+            libro.get().autores().stream().collect(Collectors.toList()).forEach(a -> {
+                System.out.println("\t nombre: " + a.nombre());
+                System.out.println("\t nacimiento: " + a.fechaDeNacimiento());
+                System.out.println("\t fallecimiento: " + a.fechaDeFallecimiento());
+            });
+            System.out.println("Idiomas :");
+            for (String idioma : libro.get().idiomas()) {
+                System.out.println("\t - " + idioma);
+            }
+            System.out.println("Total de descargas : "+libro.get().descargas());
+        } else {
+            System.out.println("Libor NO Encontrado....");
+        }
     }
 }
